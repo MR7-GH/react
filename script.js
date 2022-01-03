02cf2144d0102638a6528546fd30677dc070e021
alinkkh9@gmail.com <btpacetttcctw1>
2022-01-03 23:51:46 +0330

    .

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "3vw";
+
+
+
+function myFunction() {
+    console.log(1);
+    console.log(document.getElementById('qad')[0].value);
+    document.querySelector('.BMILabel').innerHTML() = document.getElementById('qad')[0].value / (document.getElementById('vazn')[0].value) * (document.getElementById('vazn')[0].value);
+}
