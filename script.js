dd4e48156f6bc140c68c3694a9e431344590b05a
alinkkh9@gmail.com <btpacetttcctw1>
2022-01-04 00:02:44 +0330

    .

--- script.js
+++ script.js
@@ -4,6 +4,37 @@ document.getElementsByTagName("h1")[0].style.fontSize = "3vw";
 
 function myFunction() {
     console.log(1);
-    console.log(document.getElementById('qad')[0].value);
-    document.querySelector('.BMILabel').innerHTML() = document.getElementById('qad')[0].value / (document.getElementById('vazn')[0].value) * (document.getElementById('vazn')[0].value);
+
+    let height = parseInt(document
+        .querySelector("#qad").value);
+
+    let weight = parseInt(document
+        .querySelector("#vazn").value);
+
+    let result = document.querySelector(".AnswerBox");
+
+    if (height === "" || isNaN(height))
+        result.innerHTML = "Provide a valid Height!";
+
+    else if (weight === "" || isNaN(weight))
+        result.innerHTML = "Provide a valid Weight!";
+
+    // If both input is valid, calculate the bmi
+    else {
+
+        // Fixing upto 2 decimal places
+        let bmi = (weight / ((height * height) /
+            10000)).toFixed(2);
+
+        // Dividing as per the bmi conditions
+        if (bmi < 18.6) result.innerHTML =
+            `Under Weight : <span>${bmi}</span>`;
+
+        else if (bmi >= 18.6 && bmi < 24.9)
+            result.innerHTML =
+            `Normal : <span>${bmi}</span>`;
+
+        else result.innerHTML =
+            `Over Weight : <span>${bmi}</span>`;
+    }
 }
